1bbadbfb4239e4665808e88193836da41ad19170
ingdavann <ingdavann4444@gmail.com>
2025-06-16 08:58:44 +0700

    Render card with dynamic route

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,10 +1,27 @@
+import ProductCard from '@/components/product/ProductCard';
 import React from 'react'
+import { products } from '../data/product';
 
 export default async function page() {
-await new Promise(resolve => setTimeout(resolve, 5000)); // Simulate a delay
+  await new Promise(resolve => setTimeout(resolve, 1000)); // Simulate a delay
   return (
-    <div>
-      Product Page
-    </div>
+    <section className='w-[90%] mx-auto my-10'>
+      <h2 className='font-bold text-[24px] text-blue-500 uppercase'>Product Page</h2>
+      <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4'>
+        {
+          products.map((product) => (
+            <ProductCard
+              key={product.id}
+              id={product.id}
+              name={product.name}
+              description={product.description}
+              price={product.price}
+              imageUrl={product.imageUrl}
+              category={product.category}
+            />
+          ))
+        }
+      </div>
+    </section>
   )
 }
